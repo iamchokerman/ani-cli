c40b715aca0aecd424a024f5835833029911096b
chokerman <ivan@chokerman.local>
2022-04-18 07:48:06 +0200

    feat: pypresence thumbnail

--- coolkidzonly.py
+++ coolkidzonly.py
@@ -3,11 +3,14 @@ from pypresence import Presence
 import time
 import sys
 import subprocess
+import httpx
 
 client_id = "963136145691140097"
 RPC = Presence(client_id)
 RPC.connect()
-IMAGE = "https://img.youtube.com/vi/V2lXQ2iTr0A/0.jpg"
+content_title, *gibberish = sys.argv[1].rsplit(" ", 1)
+print(content_title)
+IMAGE = httpx.get(f"https://kitsu.io/api/edge/anime?filter[text]={content_title}").json()["data"][0]["attributes"]["posterImage"]["medium"]
 ACTIVITY = sys.argv[1]+" "+sys.argv[2]
 executable = sys.argv[5]
 process = subprocess.Popen(args=[executable, sys.argv[3],
